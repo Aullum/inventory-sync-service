5cce10e7df141016cadaef423a828db47e186500
bond.zidinov@gmail.com <bond.zidinov@gmail.com>
2025-12-14 19:36:45 +0200

    add fastapi lifespan main

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    pass
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.infrastructure.config import load_config
+from app.infrastructure.http.client import build_httpx_client
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.config = load_config()
+    app.state.http = build_httpx_client()
+
+    try:
+        yield
+    finally:
+        await app.state.http.aclose()
+
+
+app = FastAPI(lifespan=lifespan)
